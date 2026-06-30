5196121c35e3f02d4c2e2f4d03c40a14af3b55cb
Jessa Bañadera <162103229+Sseej@users.noreply.github.com>
2026-06-30 19:49:56 +0800

    Add scroll to top button functionality

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Portfolio Loaded!");
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+
+        scrollBtn.style.display = "block";
+
+    }else{
+
+        scrollBtn.style.display = "none";
+
+    }
+
+});
+
+scrollBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
